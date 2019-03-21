19aaab97a76e3f69a218536e2a9add6d1bb3d7dd
Qiaoyi Li <37761636+li992@users.noreply.github.com>
2019-03-20 22:40:37 -0600

    Connection File
    Add Connection File to SQL server

---
+++ ConnectionFile/Connection.php
@@ -0,0 +1,17 @@
+<? php
+$serverName="hivm.database.windows.net";
+    $connectionOptions = array(
+   		 "Database"=> "HIVM",
+   		 "UID"=>"GroupUser",
+   		 "PWD"=>"hivm-admin01"
+    );
+    
+    $conn = sqlsrv_connect($serverName,$connectionOptions);
+    if($conn){
+   	 echo"connection established.<br/>";
+    }
+    else{
+   	 echo"Connection could not be established.<br/>";
+   	 die(print_r(sqlsrv_errors(),true));
+    }
+?>
