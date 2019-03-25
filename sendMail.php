ca4ffd263050ebefee3cd0a54e8bcf1ae6a66f4f
li992 <37761636+li992@users.noreply.github.com>
2019-03-24 20:40:08 -0600

    Mailer Update

--- sendMail.php
+++ sendMail.php
@@ -1,35 +1,37 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
-include_once 'PHPMailer/PHPMailer/Exception.php';
-include_once 'PHPMailer/PHPMailer/SMTP.php';
-include_once 'PHPMailer/PHPMailer/Exception.php';
+include_once 'PHPMailer/Exception.php';
+include_once 'PHPMailer/SMTP.php';
+include_once 'PHPMailer/Exception.php';
 
-$mail = new PHPMailer(true);
-
-try {
-    //Server settings
-    //$mail->SMTPDebug = 2;                                       // Enable verbose debug output
-    //$mail->isSMTP();                                            // Set mailer to use SMTP
-    $mail->Host       = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'qiaoyi096@example.com';                     // SMTP username
-    $mail->Password   = 'lqY199612091814';                               // SMTP password
-    $mail->SMTPSecure = 'ssl';                                  // Enable TLS encryption, `ssl` also accepted
-    $mail->Port       = 465;                                    // TCP port to connect to
+$mail = new PHPMailer();
     
-    //Recipients
-    $mail->setFrom('no-reply@hivm.com', 'Sender');
-    $mail->addAddress('mus.jonny.lee@gmail.com');     // Add a recipient       
-    $mail->addCC('qiaoyi096@gmail.com');
-
-    // Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+$mail->Host       = 'smtp.gmail.com';  // Specify main and backup SMTP servers
     
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-}
+$mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    
+$mail->Username   = 'qiaoyi096@example.com';                     // SMTP username
+    
+$mail->Password   = 'lqY199612091814';                               // SMTP password
+    
+$mail->SMTPSecure = 'ssl';                                  // Enable TLS encryption, `ssl` also accepted
+    
+$mail->Port       = 465;                                    // TCP port to connect to
+    
+$mail->setFrom('no-reply@hivm.com', 'Sender');
+    
+$mail->addAddress('mus.jonny.lee@gmail.com');     // Add a recipient       
+    
+$mail->addCC('qiaoyi096@gmail.com');
+
+    
+$mail->isHTML(true);                                  // Set email format to HTML
+    
+$mail->Subject = 'Here is the subject';
+    
+$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    
+    
+$mail->send()
